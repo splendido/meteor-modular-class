1be3e7bc6920672866582ad6edc5081efc818d10
Luca Mussi <ing.luca.mussi@gmail.com>
2014-11-28 11:22:10 +0100

    better initialization pattern

--- packages/main-a/lib/main.js
+++ packages/main-a/lib/main.js
@@ -3,3 +3,9 @@
 //  Logs the start of execution of this file
 // ------------------------------------------
 ModularClassLog.debug('Executing modular:main-a:main.js');
+
+// --------------------------
+//  Base Class Instantiation
+// --------------------------
+
+ModularClass = new ModularBC();
--- packages/main-a/package.js
+++ packages/main-a/package.js
@@ -10,11 +10,13 @@ Package.onUse(function(api) {
 
   api.use('modular:base@1.0.0');
   api.use('modular:plugin@1.0.0', { weak: true });
-  api.use('modular:init@1.0.0', { unordered: true });
+  api.use('modular:init@1.0.0');
 
   api.addFiles([
     'lib/main.js',
   ], ['server', 'client']);
+
+  api.export('ModularClass', ['client', 'server']);
 });
 
 Package.onTest(function(api) {
